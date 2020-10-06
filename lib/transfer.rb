baeebbe1c822741b0511bdfbf540a49c62203203
Erick Castille <ecastille924@gmail.com>
2020-10-06 21:57:09 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,10 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :payment, :status
+  
+  def initialize (sender, receiver, payment)
+    @sender = sender
+    @receiver = receiver
+    @payment = payment
+    @status = "pending"
+  end
 end
