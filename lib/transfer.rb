71e0fb75672bdf8d31ca64d54e8c920ea13e4404
Erick Castille <ecastille924@gmail.com>
2020-10-06 22:32:30 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
   end
   
   def execute_transaction
-    if @sender.balance > @amount 
+    if @sender.balance > @amount && status == "pending"
       @sender.deposit (@amount * -1)
       @receiver.deposit (@amount)
       @status = "complete"
