58e55caa5e5ad5d99e9a52694e4dab3e5c07638b
Erick Castille <ecastille924@gmail.com>
2020-10-06 22:46:32 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,7 +21,7 @@ class Transfer
   def execute_transaction
     if @sender.balance < @amount
       @status = "rejected"
-      "complete"
+      "Transaction rejected. Please check your account balance."
     elsif
       @sender.balance > @amount && @status == "pending"
       @sender.deposit (@amount * -1)
